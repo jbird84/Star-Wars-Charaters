cb74ca60de35292b0d302a9fffe5c6e4ed5c9e3f
Josh Kinney <joshkinney@kinneykare.com>
2019-12-12 20:42:55 -0600

    Initial Commit

---
+++ Star Wars Charaters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Wars Charaters
+//
+//  Created by Kinney Kare on 12/12/19.
+//  Copyright © 2019 Kinney Kare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

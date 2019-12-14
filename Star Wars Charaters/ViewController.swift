333a81be8144226b2603204eb7aee43e6ed2cd55
Josh Kinney <joshkinney@kinneykare.com>
2019-12-14 12:39:58 -0600

    Added TableView, Character VC, and set up TableView

--- Star Wars Charaters/ViewController.swift
+++ Star Wars Charaters/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let nameArray = ["BB-8 R2-D2 C-3PO", "Boba Fett", "Chewbacca", "Darth Maul", "Darth Vader", "Emperor Palpatine", "Han Solo", "Kylo Ren", "Luke Skywalker", "Obi Wan Kenobi", "Princess Leia", "Rey", "Yoda"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate  {
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    
+    
+}

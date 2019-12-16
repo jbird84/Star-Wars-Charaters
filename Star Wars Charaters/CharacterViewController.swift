e2002597184d742e50581cb8fbd14d9b585ef579
Josh Kinney <joshkinney@kinneykare.com>
2019-12-15 18:30:53 -0600

    finished the app. Now you can tap on any character from the table view and it will display the character's name, image and description in a new view.

--- Star Wars Charaters/CharacterViewController.swift
+++ Star Wars Charaters/CharacterViewController.swift
@@ -12,6 +12,10 @@ class CharacterViewController: UIViewController {
     
     var characterName = ""
     var imageArray = [UIImage]()
+    let charaterBackground = AboutCharacter()
+    
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var aboutTextView: UITextView!
     
     override func viewDidLoad() {
         self.title = characterName
@@ -30,9 +34,49 @@ class CharacterViewController: UIViewController {
         imageArray.append(UIImage(named: "Rey")!)
         imageArray.append(UIImage(named: "Yoda")!)
         
-        
+     
+        switch title {
+        case "BB-8 R2-D2 C-3PO":
+            characterImageView.image = imageArray[0]
+            aboutTextView.text = AboutCharacter.Character.BB8R2D2C3PO.rawValue
+        case "Boba Fett":
+        characterImageView.image = imageArray[1]
+        aboutTextView.text = AboutCharacter.Character.BobaFett.rawValue
+        case "Chewbacca":
+        characterImageView.image = imageArray[2]
+        aboutTextView.text = AboutCharacter.Character.Chewbacca.rawValue
+        case "Darth Maul":
+        characterImageView.image = imageArray[3]
+        aboutTextView.text = AboutCharacter.Character.DarthMaul.rawValue
+        case "Darth Vader":
+        characterImageView.image = imageArray[4]
+        aboutTextView.text = AboutCharacter.Character.DarthVader.rawValue
+        case "Emperor Palpatine":
+        characterImageView.image = imageArray[5]
+        aboutTextView.text = AboutCharacter.Character.EmperorPalpatine.rawValue
+        case "Han Solo":
+        characterImageView.image = imageArray[6]
+        aboutTextView.text = AboutCharacter.Character.HanSolo.rawValue
+        case "Kylo Ren":
+        characterImageView.image = imageArray[7]
+        aboutTextView.text = AboutCharacter.Character.KyloRen.rawValue
+        case "Luke Skywalker":
+        characterImageView.image = imageArray[8]
+        aboutTextView.text = AboutCharacter.Character.LukeSkywalker.rawValue
+        case "Obi Wan Kenobi":
+        characterImageView.image = imageArray[9]
+            aboutTextView.text = AboutCharacter.Character.ObiWanKenobi.rawValue
+        case "Princess Leia":
+        characterImageView.image = imageArray[10]
+        aboutTextView.text = AboutCharacter.Character.PrincessLeia.rawValue
+        case "Rey":
+        characterImageView.image = imageArray[11]
+        aboutTextView.text = AboutCharacter.Character.Rey.rawValue
+        case "Yoda":
+        characterImageView.image = imageArray[12]
+        aboutTextView.text = AboutCharacter.Character.Yoda.rawValue
+        default:
+        print("Error with case Statement")
+        }
     }
-    
-    
-    
 }
